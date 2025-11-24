03628d4520edd025b46adf57e240d3a52d30e618
hwnooy <heejudgit@gmail.com>
2025-11-25 00:45:35 +0900

    initial commit

---
+++ src/main/java/com/org/myMealScanner/MyMealScannerApplication.java
@@ -0,0 +1,13 @@
+package com.org.myMealScanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMealScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMealScannerApplication.class, args);
+	}
+
+}
