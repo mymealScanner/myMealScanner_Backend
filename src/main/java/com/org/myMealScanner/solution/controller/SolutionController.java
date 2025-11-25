ed7adcaf317cda1c74b1dd9363fa50a8873bc3d6
hwnooy <heejudgit@gmail.com>
2025-11-26 01:49:42 +0900

    feat: Solution Controller API 구현 완료

---
+++ src/main/java/com/org/myMealScanner/solution/controller/SolutionController.java
@@ -0,0 +1,23 @@
+package com.org.myMealScanner.solution.controller;
+
+import com.org.myMealScanner.solution.dto.NutritionRequestDto;
+import com.org.myMealScanner.solution.dto.NutritionSolutionResponse;
+import com.org.myMealScanner.solution.service.SolutionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/solution")
+@RequiredArgsConstructor
+public class SolutionController {
+    private final SolutionService solutionService;
+
+    @PostMapping("")
+    public NutritionSolutionResponse getAdvice(@RequestBody NutritionRequestDto request) throws Exception {
+        return solutionService.getAdvice(request);
+    }
+
+}
