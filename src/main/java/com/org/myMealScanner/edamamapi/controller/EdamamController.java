a9979ab2af0deffb0b84153ba12de7c798f16f30
hyejin <hyejin66@sookmyung.ac.kr>
2025-11-25 16:33:53 +0900

    feat: 특정 음식에 대한 영양 정보, 특정 레시피에 대한 영양 정보 응답해주는 기능 추가 (#1)

--- src/main/java/com/org/myMealScanner/edamamapi/controller/EdamamController.java
+++ src/main/java/com/org/myMealScanner/edamamapi/controller/EdamamController.java
@@ -1,4 +1,37 @@
 package com.org.myMealScanner.edamamapi.controller;
 
+import com.org.myMealScanner.edamamapi.dto.EdamamRecipeRequestDto;
+import com.org.myMealScanner.edamamapi.dto.EdamamResponseDto;
+import com.org.myMealScanner.edamamapi.dto.MacroNutrientsDto;
+import com.org.myMealScanner.edamamapi.service.EdamamRecipeService;
+import com.org.myMealScanner.edamamapi.service.EdamamService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
 public class EdamamController {
+    private final EdamamService edamamService;
+    private final EdamamRecipeService edamamRecipeService;
+
+    @GetMapping("/nutrition/{foodName}")
+    public EdamamResponseDto analyzeNutrition(@PathVariable String foodName) {
+        return edamamService.getNutritionInfo(foodName);
+    }
+
+    @GetMapping("/analysis/{foodName}")
+    public MacroNutrientsDto analyzeAnalysis(@PathVariable String foodName) {
+        return edamamService.getMacroNutrition(foodName);
+    }
+    @PostMapping("/nutrition/analyze-recipe")
+    public EdamamResponseDto analyzeRecipeNutrition(
+            @RequestBody EdamamRecipeRequestDto requestDto) {
+        return edamamRecipeService.analyzeRecipeNutrition(requestDto);
+    }
+    @PostMapping("/nutrition/summary-recipe")
+    public MacroNutrientsDto analyzeRecipeSummary(
+            @RequestBody EdamamRecipeRequestDto requestDto) {
+        return edamamRecipeService.getMacroNutritionSummary(requestDto);
+    }
+
 }
