2012ba5bb77b276eb0d1f96de676f16d374626a4
hwnooy <heejudgit@gmail.com>
2025-11-26 01:45:16 +0900

    feat: 영양 분석/추천을 위한 requestDto, 솔루션 응답 responseDto 생성

---
+++ src/main/java/com/org/myMealScanner/solution/dto/NutritionRequestDto.java
@@ -0,0 +1,8 @@
+package com.org.myMealScanner.solution.dto;
+
+import lombok.Getter;
+
+@Getter
+public class NutritionRequestDto {
+    private String nutritionInfo;
+}
---
+++ src/main/java/com/org/myMealScanner/solution/dto/NutritionSolutionResponse.java
@@ -0,0 +1,10 @@
+package com.org.myMealScanner.solution.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class NutritionSolutionResponse {
+    private String solutionInfo;
+}
