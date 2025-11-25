8056204d9413df60cd818ac649f568fafbd3371a
hyejin <hyejin66@sookmyung.ac.kr>
2025-11-25 16:34:18 +0900

    feat: webclientconfig 추가 (#1)

--- src/main/java/com/org/myMealScanner/edamamapi/config/WebClientConfig.java
+++ src/main/java/com/org/myMealScanner/edamamapi/config/WebClientConfig.java
@@ -1,4 +1,13 @@
 package com.org.myMealScanner.edamamapi.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class WebClientConfig {
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
 }
